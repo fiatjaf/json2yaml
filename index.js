d4a15690717b809f66fe1ff8efd3fdcc1f7c5391
fiatjaf@spooner <fiatjaf@gmail.com>
2014-08-21 17:25:35 -0400

    prettier and safer output

--- index.js
+++ index.js
@@ -4,9 +4,18 @@
   var typeOf = require('remedial').typeOf
     ;
 
+  var indent = function (level) {
+    var spaces = '';
+    for (var i = 0; i < level; i++) {
+      spaces += '  ';
+    }
+    return spaces;
+  }
+
   function stringify(data) {
     var handlers
-      , indentLevel = ''
+      , indentLevel = -1
+      , initial = true
       ;
 
     handlers = {
@@ -26,9 +35,23 @@
           return x ? 'true' : 'false';
         }
       , "string": function (x) {
-          // to avoid the string "true" being confused with the
-          // the literal `true`, we always wrap strings in quotes
-          return JSON.stringify(x);
+          // if the string has [], {}, \n, \t,  or is a enquoted number, use quotes
+          if (
+            (x.indexOf('[') == 0) ||
+            (x.indexOf(']') == x.length-1) ||
+            (x.indexOf('{') == 0) ||
+            (x.indexOf('}') == x.length-1) ||
+            (x.indexOf('\n') != -1) ||
+            (x.indexOf('\t') != -1) ||
+            (x == 'true') ||
+            (x == 'false') ||
+            (!isNaN(parseFloat(x)) && isFinite(x))
+          ) {
+            return JSON.stringify(x);
+          }
+          else {
+            return x;
+          }
         }
       , "array": function (x) {
           var output = ''
@@ -39,20 +62,21 @@
             return output;
           }
 
-          indentLevel = indentLevel.replace(/$/, '  ');
+          indentLevel++;
           x.forEach(function (y) {
             // TODO how should `undefined` be handled?
             var handler = handlers[typeOf(y)]
               ;
 
             if (!handler) {
-              throw new Error('what the crap: ' + typeOf(y));
+              throw new Error('cannot encode this: ' + typeOf(y));
             }
 
-            output += '\n' + indentLevel + '- ' + handler(y);
+            output += (initial ? '' : '\n') + indent(indentLevel) + '- ' + handler(y);
+            initial = false;
              
           });
-          indentLevel = indentLevel.replace(/  /, '');
+          indentLevel--;
           
           return output;
         }
@@ -65,7 +89,7 @@
             return output;
           }
 
-          indentLevel = indentLevel.replace(/$/, '  ');
+          indentLevel++;
           Object.keys(x).forEach(function (k) {
             var val = x[k]
               , handler = handlers[typeOf(val)]
@@ -81,12 +105,19 @@
             }
 
             if (!handler) {
-              throw new Error('what the crap: ' + typeOf(val));
+              throw new Error('cannot encode this: ' + typeOf(val));
             }
 
-            output += '\n' + indentLevel + k + ': ' + handler(val);
+            // enquote the key if it is not a so recognizable string as it seems
+            if (!(/^\w+$/.exec(k))) {
+              k.replace(/"/g, '\\"');
+              k = '"' + k + '"';
+            }
+
+            output += (initial ? '' : '\n') + indent(indentLevel) + k + ': ' + handler(val);
+            initial = false;
           });
-          indentLevel = indentLevel.replace(/  /, '');
+          indentLevel--;
 
           return output;
         }
@@ -96,7 +127,7 @@
         }
     };
 
-    return '---' + handlers[typeOf(data)](data) + '\n';
+    return handlers[typeOf(data)](data);
   }
 
   module.exports.stringify = stringify;
